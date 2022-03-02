ab7afe2b3f6f8480d89d90726e2bdcaab7ee1457
Olya <anoshina.bayarea@gmail.com>
2022-03-02 13:01:07 -0800

    complete task 2

--- Homework2/ViewController.swift
+++ Homework2/ViewController.swift
@@ -9,11 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    @IBOutlet var startBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        redLight.alpha = 0.3
+        redLight.layer.cornerRadius = 100
+        yellowLight.alpha = 0.3
+        yellowLight.layer.cornerRadius = 100
+        greenLight.alpha = 0.3
+        greenLight.layer.cornerRadius = 100
+        startBtn.layer.cornerRadius = 20
     }
 
-
+    @IBAction func startBtnPressed() {
+        if redLight.alpha < 1 && yellowLight.alpha < 1 && greenLight.alpha < 1 {
+            redLight.alpha = 1
+            print("pressed")
+        } else if redLight.alpha == 1 {
+            redLight.alpha = 0.3
+            yellowLight.alpha = 1
+        } else if yellowLight.alpha == 1 {
+            yellowLight.alpha = 0.3
+            greenLight.alpha = 1
+        } else if greenLight.alpha == 1 {
+            greenLight.alpha = 0.3
+            redLight.alpha = 1
+        }
+        startBtn.setTitle("NEXT", for: .normal)
+    }
+    
 }
 
